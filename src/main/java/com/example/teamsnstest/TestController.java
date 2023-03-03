daeffe71afb57563153b6b7993dfa5001754d992
Dongvin,Park <dongvin99@naver.com>
2023-03-03 21:02:23 +0900

    JPQL 에서 LocalDateTime과 IN 쿼리 사용 성공

--- src/main/java/com/example/teamsnstest/TestController.java
+++ src/main/java/com/example/teamsnstest/TestController.java
@@ -7,13 +7,13 @@ import com.example.teamsnstest.persist.FeedRepository;
 import com.example.teamsnstest.persist.User;
 import com.example.teamsnstest.persist.UserRepository;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +26,7 @@ public class TestController {
 
     @GetMapping("/run")
     @Transactional
-    public void run(){
+    public void run() throws InterruptedException {
 
         // 유저를 3명 만든다.
         User user = User.builder()
@@ -56,11 +56,16 @@ public class TestController {
         System.out.println("유저 3명 세이브 완료.");
 
         // 게시물을 2개 만든다. 이때 1번 유저를 넣어서 양방향 매핑을 활용한다.
+        // 두 번째 게시물은 10초의 시간을 두고 만든다.
         Feed feed = Feed.builder()
             .content("게시물 내용")
             .user(user)
             .imageUrls("이미지1,이미지2")
             .build();
+
+        feedRepository.save(feed);
+
+        Thread.sleep(1000*10);
 
         Feed feed2 = Feed.builder()
             .content("게시물 내용2")
@@ -68,8 +73,15 @@ public class TestController {
             .imageUrls("이미지1,이미지2")
             .build();
 
-        feedRepository.save(feed);
+        Feed feed3 = Feed.builder()
+            .content("게시물 내용3")
+            .user(user3)
+            .imageUrls("이미지1,이미지2")
+            .build();
+
         feedRepository.save(feed2);
+        feedRepository.save(feed3);
+
         System.out.println("게시물 세이브 완료.");
 
     }// func
@@ -77,10 +89,18 @@ public class TestController {
     @GetMapping("/read")
     public List<TestFeedResponse> makeResponse(){
         // 게시물 엔티티를 불러와서 리스펀스를 만들 때, 유저도 같이 참조하게 만든다.
-        PageRequest pageRequest = PageRequest.of(0, 1);
+        PageRequest pageRequest = PageRequest.of(0, 2, Sort.by("id").descending());
 
-        return feedRepository.findAllByJoinUser().getContent().stream()
-            .map(TestFeedResponse::secondFrom).collect(Collectors.toList());
+        List<Long> feedIdList = new ArrayList<>();
+        feedIdList.add(2L);
+
+        LocalDateTime to = LocalDateTime.now();
+        LocalDateTime from = to.minusSeconds(5);
+
+        return feedRepository.findAllByJoinUserWithTimeDurationAndFeedIdListAndPaging(
+            feedIdList, from, to, pageRequest
+            ).getContent().stream().map(TestFeedResponse::secondFrom)
+            .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/example/teamsnstest/persist/FeedRepository.java
+++ src/main/java/com/example/teamsnstest/persist/FeedRepository.java
@@ -1,11 +1,13 @@
 package com.example.teamsnstest.persist;
 
 import com.example.teamsnstest.converter.ForTestResponse;
-import com.example.teamsnstest.dto.ForTestResponseImpl;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,17 +20,64 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
     @Query(
         value =
             "SELECT"
+                + " f.id as feedId,"
+                + " f.content as content,"
+                + " f.imageUrls as imageUrl,"
+                + " u.username as username, "
+                + " u.profileImageUrl as profileImageUrl,"
+                + " u.nickname as nickname"
+                + " FROM Feed f"
+                + " JOIN User u"
+                + " ON f.user.id = u.id"
+    )
+    Slice<ForTestResponse> findAllByJoinWithNoPaging(
+        @Param("from") LocalDateTime from,
+        @Param("to") LocalDateTime to
+    );
+
+    @Query(
+        value =
+            "SELECT"
             + " f.id as feedId,"
             + " f.content as content,"
             + " f.imageUrls as imageUrl,"
             + " u.username as username, "
             + " u.profileImageUrl as profileImageUrl,"
-            + " u.nickname as nickname" +
-        " FROM Feed f" +
-        " JOIN User u" +
-        " ON f.user.id = u.id"
+            + " u.nickname as nickname"
+                + " FROM Feed f"
+                + " JOIN User u"
+                + " ON f.user.id = u.id"
+        + " WHERE f.createdAt >= :from "
+        + " AND f.createdAt <= :to "
     )
-    Slice<ForTestResponse> findAllByJoinUser();
+    Slice<ForTestResponse> findAllByJoinUserWithTimeDurationAndPaging(
+        @Param("from") LocalDateTime from,
+        @Param("to") LocalDateTime to,
+        PageRequest pageRequest
+    );
+
+    @Query(
+        value =
+            "SELECT"
+                + " f.id as feedId,"
+                + " f.content as content,"
+                + " f.imageUrls as imageUrl,"
+                + " u.username as username, "
+                + " u.profileImageUrl as profileImageUrl,"
+                + " u.nickname as nickname"
+                + " FROM Feed f"
+                + " JOIN User u"
+                + " ON f.user.id = u.id"
+                + " WHERE f.id in (:feedIdList) "
+                + " AND f.createdAt >= :from "
+                + " AND f.createdAt <= :to "
+    )
+    Slice<ForTestResponse> findAllByJoinUserWithTimeDurationAndFeedIdListAndPaging(
+        @Param("feedIdList") List<Long> feedIdList,
+        @Param("from") LocalDateTime from,
+        @Param("to") LocalDateTime to,
+        PageRequest pageRequest
+    );
 
 }
 
